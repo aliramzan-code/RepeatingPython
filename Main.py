308df7e7d8ba3029b8fdc77db39699771c736912
AliRamzan <130931946+aliramzan-code@users.noreply.github.com>
2023-04-25 10:34:58 +0500

    Variables and Data Types

---
+++ Main.py
@@ -0,0 +1,26 @@
+#Variables :
+    #variables are containers that store data values.
+    #For Example X = "Python"
+    #in python you have to assign value to variable at first
+    #In python you do't need to define the type of variable
+x = 4
+print(x)
+    #and at the same time you can change the type of variable by just changing the value
+x = "Python"
+print(x)
+    #You can specifiy the data type using casting 
+x = str(4)
+x = int(4)
+x = str(4)
+x = float(4)
+
+    #Casting is a process of covernting data type to another data type 
+    #if you want to know the type of variable you can use type() method
+y = "Hi"
+print(type(x))
+print(type(y))
+    #the output will : <class 'float'>
+    #<class 'str'>
+    #Strings are sequnce of characters it cantains numbers,letters , symbols and even speces
+    #  
+
