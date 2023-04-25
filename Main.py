4ff58f8539a335a4161d21a6f533673eeef9866e
Ali Ramzan <aliramzan.code@gmail.com>
2023-04-25 11:55:05 +0500

    initial commit

--- Main.py
+++ Main.py
@@ -22,5 +22,20 @@ print(type(y))
     #the output will : <class 'float'>
     #<class 'str'>
     #Strings are sequnce of characters it cantains numbers,letters , symbols and even speces
-    #  
+    #Everything in single and double quoutes or String in python 
+    #Python is case-sensitve langauge 
+    #for example 
+a = 3
+A = "Python"
+    #these are two different variables
+
+    #There are some Python rules for decarling Variables 
+    # Rules
+    # 1.  A variable must start with a letter or underscore character.
+    # 2.  A variable cannot start with number .
+    # 3.  A varibale name can only contain alpha-numeric characters and underscores(A-Z,0-9,_)
+    # 4.  A varibale name is case-sensitive(age, Age , AGE are three different names)
+    # 5.  A variable name cannot be any of Python-Keyword
+    
+
 
